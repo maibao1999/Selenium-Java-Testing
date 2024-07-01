feb517f3c34b11e8be7560c8d536d2b9145fc544
MaiBao <baoa4madrid@gmail.com>
2024-07-01 10:46:08 +0700

    The first commit

---
+++ Selenium/webdriver/CheckEnv.java
@@ -0,0 +1,49 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class CheckEnv {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+
+    @BeforeClass
+    public void beforeClass(){
+//    //    System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\Desktop\\Learn - Selenium\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+//        driver = new ChromeDriver();
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+
+    @Test
+    public void TC_01_Run_On_Firefox() {
+        driver = new FirefoxDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+
+    @Test
+    public void TC_02_Run_On_Chrome() {
+        driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+
+    @Test
+    public void TC_03_Run_On_Edge() {
+        driver = new EdgeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+
+    @AfterClass
+    public void afterClass(){
+        driver.quit();
+    }
+}
